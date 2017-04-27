4001dcdcc30ab575ebbf462a0d82c3981cc01893
다영 <oongjjong@gmail.com>
2017-04-27 21:25:47 +0900

    Mapping 성공 http://localhost:8080/SIRORAGI/hello/으로 실행하면 됩니다.

--- src/main/java/spring/siroragi/common/CommonController.java
+++ src/main/java/spring/siroragi/common/CommonController.java
@@ -1,5 +1,23 @@
 package spring.siroragi.common;
 
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
 public class CommonController {
 
+	//실험용
+	@RequestMapping(value="/hello/")
+	public ModelAndView list() {
+		ModelAndView mv = new ModelAndView("home");
+
+    	return mv;
+	}
 }
