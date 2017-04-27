05acda5a620efe56f1075fab516fd700dc4615ef
다영 <oongjjong@gmail.com>
2017-04-28 04:26:03 +0900

    Tiles 설정(pom.xml,action-servlet.xml에서 viewResolver설정함)  Tiles.xml에서 설정하면 됨(pet참조), 관리자페이지 /admin/adminPage로 이동까지만 가능

--- src/main/java/spring/siroragi/admin/AdminController.java
+++ src/main/java/spring/siroragi/admin/AdminController.java
@@ -1,5 +1,15 @@
 package spring.siroragi.admin;
 
-public class AdminController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class AdminController {
+	
+	//관리자페이지로 이동
+	@RequestMapping(value="/admin/adminPage")
+	public String list() {
+		return "adminForm";
+	}
 }
